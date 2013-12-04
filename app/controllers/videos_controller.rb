8aeac42ad046a0ae878cbe95921ffb4e200dea81
MakerSquare Student <students@themakersquare.com>
2013-12-04 11:28:53 -0600

    Added strong params in order to fix error. Full functionality, huzzah

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -10,13 +10,20 @@ class VideosController < ApplicationController
   end
 
   def create
-  	@video = Video.new(params)
+  	@video = Video.new(videos_params)
   	@video.save
   	redirect_to root_path
   end
 
+
   def show_all
     @videos = Video.all
   end
+
+  private
+	def videos_params
+  		params.require(:video).permit(:title, :description, :youtube_id)
+	end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ MetubeCohort3::Application.routes.draw do
   root 'videos#show_all'
   get '/videos' => 'videos#show_all'
   get '/videos/:id' => 'videos#show', as: 'video'
-  post '/videos' => 'video#create'
+  post '/videos' => 'videos#create'
 
 end
