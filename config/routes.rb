2bfda34f4c8cc91d64881064f14e33d52a6ab4ea
MakerSquare Student <students@themakersquare.com>
2013-12-04 10:56:24 -0600

    Added a create method in videos_controller and corresponding POST route.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ MetubeCohort3::Application.routes.draw do
   root 'videos#show_all'
   get '/videos' => 'videos#show_all'
   get '/videos/:id' => 'videos#show', as: 'video'
+  post '/videos' => 'video#create'
 
 end
