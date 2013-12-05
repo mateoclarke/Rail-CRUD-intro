daee554a6e4c5ac4a200e0695a54b51f37eaad83
mateoclarke <code@mateoclarke.com>
2013-12-04 18:36:46 -0600

    Added new routes for edit and update actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 MetubeCohort3::Application.routes.draw do
 
-  get '/videos/new' => 'videos#new', as: 'new_video'
   root 'videos#show_all'
+  get '/videos/new' => 'videos#new', as: 'new_video'
   get '/videos' => 'videos#show_all'
   get '/videos/:id' => 'videos#show', as: 'video'
+  get '/video/:id/edit' => 'videos#edit', as: 'edit_video'
   post '/videos' => 'videos#create'
+  put '/videos/:id' => 'videos#update'
+  patch '/videos/:id' => 'videos#update'
 
 end
